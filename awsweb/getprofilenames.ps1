cb15e9e27f550045218b034a401acf005bc3904d
kahgeh.tan <kahgeh.tan@xero.com>
2019-02-28 14:33:59 +1100

    make get profile names script slightly better by providing group name and matching just once

--- awsweb/getprofilenames.ps1
+++ awsweb/getprofilenames.ps1
@@ -1,5 +1,10 @@
 #!/usr/bin/env pwsh
 $config = Get-Content "$HOME/.aws/config"
+$pattern = [Regex]"\[(\s*profile\s+){0,1}(?<name>[^\s]+)\]"
 $config | 
-    Where-Object { $_ -match "\[(\s*profile\s+){0,1}([^\s]+)\]" } |  
-    ForEach-Object{ $_ -replace "\[(\s*profile\s+){0,1}([^\s]+)\]",'$2' }
+    ForEach-Object { 
+        $result = $pattern.Match($_)
+        if ( $result.Success ) {
+            $result.Groups['name'].Value
+        }
+    }    
