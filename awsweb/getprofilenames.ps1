c88bb2681d31733884d6000f1ffaa3b0c6815eae
kahgeh.tan <kahgeh.tan@xero.com>
2019-02-28 15:05:16 +1100

    exclude source profiles

--- awsweb/getprofilenames.ps1
+++ awsweb/getprofilenames.ps1
@@ -1,10 +1,25 @@
 #!/usr/bin/env pwsh
 $config = Get-Content "$HOME/.aws/config"
-$pattern = [Regex]"\[(\s*profile\s+){0,1}(?<name>[^\s]+)\]"
+$profileSectionHeaderPattern = [Regex]"\[(\s*profile\s+){0,1}(?<name>[^\s]+)\]"
+$sourcePattern = [Regex]"\s*[S|s]ource_profile\s*=\s*(?<name>[^\s]+)"
+$sectionHeaders = New-Object System.Collections.ArrayList
+$sourceProfileNames = New-Object System.Collections.ArrayList
 $config | 
     ForEach-Object { 
-        $result = $pattern.Match($_)
-        if ( $result.Success ) {
-            $result.Groups['name'].Value
+        $profileSectionHeaderMatch = $profileSectionHeaderPattern.Match($_)
+        if ( $profileSectionHeaderMatch.Success ) {
+            $sectionHeaders.Add($profileSectionHeaderMatch.Groups['name'].Value)|Out-Null;
+        }
+
+        $sourceMatch = $sourcePattern.Match($_)
+        if($sourceMatch.Success) {
+            $sourceProfileName = $sourceMatch.Groups['name'].Value
+            if ( $sourceProfileName -notcontains $sourceProfileNames){
+                $sourceProfileNames.Add($sourceProfileName)| Out-Null
+            }
         }
     }    
+
+Compare-Object $sectionHeaders $sourceProfileNames |
+    Where-Object {$_.SideIndicator -eq '<='} |
+    Select-Object -ExpandProperty InputObject
